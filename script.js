8d14237567e81c7097839a9fa99e0526f61e503e
Abhishekdubey811 <ad663429@gmail.com>
2025-11-15 10:36:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent;
+
+    if (value === "C") {
+      currentInput = "";
+      display.value = "";
+    } else if (value === "=") {
+      try {
+        display.value = eval(currentInput);
+        currentInput = display.value;
+      } catch {
+        display.value = "Error";
+        currentInput = "";
+      }
+    } else {
+      currentInput += value;
+      display.value = currentInput;
+    }
+  });
+});
